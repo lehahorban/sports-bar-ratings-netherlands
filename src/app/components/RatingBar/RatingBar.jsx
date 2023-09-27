9c33c86cf3779af2e3604194d573fc6ca1a39e9c
Oleksii Horban <lehagorban93@gmail.com>
2023-09-27 16:42:29 +0300

    fix

--- src/app/components/RatingBar/RatingBar.jsx
+++ src/app/components/RatingBar/RatingBar.jsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import RatingStar from "../RaitingStar/RaitingStar";
 import data from "../../data/sportsBar.json";
 
@@ -60,31 +61,33 @@ const RatingBar = () => {
 
         <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mt-5">
           {sortArray.map(
-            ({ name, location, description, rating, image }, ind) => (
-              <li
-                className="bg-main rounded-md px-3 py-4 flex flex-col gap-2"
-                key={ind}
-              >
-                <div className="flex flex-col justify-between flex-1 gap-1">
-                  <p className="text-orange text-xl font-bold text-center">
-                    {name}
-                  </p>
-                  <p className="text-white text-lg">{location}</p>
-                  <p className="text-accent text-base">{description}</p>
-                  <div className="flex items-center gap-4">
-                    <p className="text-white text-2xl">{rating.toFixed(1)}</p>
-                    <RatingStar value={rating} />
+            ({ name, location, description, rating, image, id }, ind) => (
+              <Link href={`/bar/${id}`} key={ind}>
+                <li
+                  className="bg-main rounded-md px-3 py-4 flex flex-col gap-2 h-full"
+                  key={ind}
+                >
+                  <div className="flex flex-col justify-between flex-1 gap-1">
+                    <p className="text-orange text-xl font-bold text-center">
+                      {name}
+                    </p>
+                    <p className="text-white text-lg">{location}</p>
+                    <p className="text-accent text-base">{description}</p>
+                    <div className="flex items-center gap-4">
+                      <p className="text-white text-2xl">{rating.toFixed(1)}</p>
+                      <RatingStar value={rating} />
+                    </div>
                   </div>
-                </div>
-                <div className="w-full h-60 relative">
-                  <Image
-                    src={image}
-                    alt={name}
-                    className="object-cover object-center"
-                    fill={true}
-                  />
-                </div>
-              </li>
+                  <div className="w-full h-60 relative">
+                    <Image
+                      src={image}
+                      alt={name}
+                      className="object-cover object-center"
+                      fill={true}
+                    />
+                  </div>
+                </li>
+              </Link>
             )
           )}
         </ul>
